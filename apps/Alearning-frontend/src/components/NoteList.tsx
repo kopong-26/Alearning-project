1e982970f079791bd03ca1211e8456444fa0d8fc
artkpt <179370395+artkpt@users.noreply.github.com>
2026-03-09 18:10:43 +0700

    beautify /notes page

--- apps/Alearning-frontend/src/components/NoteList.tsx
+++ apps/Alearning-frontend/src/components/NoteList.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router";
+
 interface NoteListProps {
     notes: any[]; // แนะนำให้สร้าง interface ของ Note แยกในอนาคตเพื่อแทนที่ any
 }
@@ -7,9 +9,12 @@ export function NoteList({notes}: NoteListProps){
     return (
         notes.map((note)=> {
             return (
-                <div className="m-4 p-2 border border-main-contrast rounded-sm h-30">
-                    <h2 className="text-xl font-semibold">{note.title}</h2>
-                    <p className="text-sm">{note.desc}</p>
+                <div key={note.id} className="m-4 p-4 border border-main-contrast rounded-sm gap-2.5 flex-col flex">
+                    <Link to={`/notes/${note.id}`} className="text-xl font-semibold text-blue-600">{note.title}</Link>
+                    <p className="text-sm ">{note.desc}</p>
+                    <p className="text-xs ">
+                        Author: {note.author} | Created On: {note.createOn}
+                    </p>
                 </div>
             )
         })
