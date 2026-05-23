27722dab7d905de626bb23bbb9e958d89a562a8d
artkpt <179370395+artkpt@users.noreply.github.com>
2026-05-23 18:54:13 +0700

    protect refresh and logout route

--- apps/alearning-backend/start/routes/auth.ts
+++ apps/alearning-backend/start/routes/auth.ts
@@ -1,6 +1,10 @@
 import AuthController from "#controllers/auth_controller";
+import { middleware } from "#start/kernel";
 import router from "@adonisjs/core/services/router";
 
 router.post('/login', [AuthController, 'login'])
-router.post('/refresh', [AuthController, 'refresh'])
-router.post('/logout', [AuthController, 'logout'])
+
+router.group(()=>{
+    router.post('/refresh', [AuthController, 'refresh'])
+    router.post('/logout', [AuthController, 'logout'])
+}).use(middleware.auth())
